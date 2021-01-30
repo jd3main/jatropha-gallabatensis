d5cdbce1c298a6de0e409b360cfdf16aaadc2708
Jamie Tu <jd3main@gmail.com>
2021-01-30 22:42:32 +0800

    PUIPUI and Shredder

---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : VoteContainer
+{
+    public float decreasePerSec = 2;
+    public float beginTime = -1;
+    public int donePieces = 0;
+
+    void Update()
+    {
+        if (beginTime == -1 && Votes > 0)
+        {
+            beginTime = Time.time;
+            donePieces = 0;
+        }
+
+        if (beginTime != -1)
+        {
+            while (Votes != 0 && donePieces < (Time.time - beginTime) * decreasePerSec)
+            {
+                donePieces++;
+                Votes--;
+            }
+            if (Votes == 0)
+            {
+                ClearState();
+            }
+        }
+    }
+
+    void ClearState()
+    {
+        beginTime = -1;
+        donePieces = 0;
+    }
+}
---
+++ Assets/Scripts/VoteContainerUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VoteContainerUI : MonoBehaviour
+{
+    [SerializeField] protected VoteContainer container;
+    [SerializeField] protected Text votesText;
+
+    void Update()
+    {
+        votesText.text = $"{container.Votes}";
+    }
+}
