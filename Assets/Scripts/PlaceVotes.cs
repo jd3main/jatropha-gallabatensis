0df32475e2f8c071bf590822ce3eab02f2ac8209
Jamie Tu <jd3main@gmail.com>
2021-01-30 20:00:50 +0800

    put votes

---
+++ Assets/Scripts/PlaceVotes.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlaceVotes : MonoBehaviour
+{
+    CharacterController character;
+    Team team;
+    VoteContainer target = null;
+
+    private void Start()
+    {
+        team = GetComponent<Team>();
+        character = GetComponent<CharacterController>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        var vc = other.GetComponent<VoteContainer>();
+        if (vc != null)
+            target = vc;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        target = null;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (target.tag == "PUIPUI")
+            {
+                if (team.TeamType == TeamType.Protector)
+                {
+                    target.Place(character);
+                }
+                else if (team.TeamType == TeamType.Thief)
+                {
+                    target.Take(character);
+                }
+            }
+            else
+            {
+                if (team.TeamType == TeamType.Protector)
+                {
+                    target.Take(character);
+                }
+                else if (team.TeamType == TeamType.Thief)
+                {
+                    target.Place(character);
+                }
+            }
+        }
+    }
+}
