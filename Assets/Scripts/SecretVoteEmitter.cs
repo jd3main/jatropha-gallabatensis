d24e9f8c5560e52db8e46543a57e6d1765076802
C.W. Chien <dppss92132@gmail.com>
2021-01-31 09:28:20 +0800

    make a room vomit and fix a I-don't-know-why null exception in PlaceVote.cs while pressing E

---
+++ Assets/Scripts/SecretVoteEmitter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretVoteEmitter : MonoBehaviour
+{
+    public GameObject VotePrefab;
+    public int amounts = 30;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = 0; i < amounts; i++)
+            {
+                GameObject ins = Instantiate(VotePrefab, transform, false);
+                ins.transform.SetParent(null, true);
+                ins.GetComponent<Vote>().Fling(Random.insideUnitCircle * 3);
+            }
+        }
+        Debug.Log("There's something weird here....hmm.....");
+    }
+
+}
